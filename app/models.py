fcac3e9a81727c65b431e3588a38630649bc3620
ChiandEruda <g549141930@gmail.com>
2019-08-01 23:38:52 +0800

    第六章：个人主页和头像

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash
 from werkzeug.security import check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 from app import db
 from app import login
@@ -15,6 +16,8 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     # 生成 “一” 对 “多” 的类，并为 Post 类反向调用添加 post.author
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
@@ -23,6 +26,14 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        # 生成用户的电子邮件地址的 MD5 哈希值
+        # Python 中的 MD5 的参数类型需要是字节而不是字符串
+        # 所以在将字符串传递给该函数之前，需要将字符串编码为字节
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
--- app/routes.py
+++ app/routes.py
@@ -8,12 +8,20 @@ from flask_login import logout_user
 from flask_login import login_required
 from flask import request
 from werkzeug.urls import url_parse
+from datetime import datetime
 
 from app import app
 from app.forms import LoginForm
 from app.models import User
 from app import db
 from app.forms import RegistrationForm
+from app.forms import EditProfileForm
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 @app.route('/')
 @app.route('/index')
@@ -73,6 +81,34 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)     
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    # 初始请求的情况
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    # 提交表单验证失败的情况
+    return render_template('edit_profile.html', title='Edit Profile',
+                        form=form)
 
 @app.route('/logout')
 def logout():
