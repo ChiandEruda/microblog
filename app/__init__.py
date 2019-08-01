10ebc23a45d19b9f44188c113181dac35c00ee85
ChiandEruda <g549141930@gmail.com>
2019-08-02 01:27:58 +0800

    第七章：错误处理

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import RotatingFileHandler
+import os
 
 from config import Config
 
@@ -14,5 +17,25 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
+if not app.debug:
+    # ...
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                    backupCount=10)
+    # logging.Formatter 类为日志消息提供自定义格式  
+    # 这里使用的格式包括时间戳、日志记录级别、消息以及日志来源的源代码文件和行号         
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+
+
 from app import routes
 from app import models
+from app import errors
+
