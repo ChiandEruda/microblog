b08bd010664d5b1a7f17218ff6dee63cfb072022
ChiandEruda <g549141930@gmail.com>
2019-08-01 22:44:47 +0800

    第五章：用户登录

--- app/forms.py
+++ app/forms.py
@@ -4,9 +4,36 @@ from wtforms import PasswordField
 from wtforms import BooleanField
 from wtforms import SubmitField
 from wtforms.validators import DataRequired
+from wtforms.validators import ValidationError
+from wtforms.validators import Email
+from wtforms.validators import EqualTo
+
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    # 当添加任何匹配模式 validate_ <field_name> 的方法时
+    # WTForms 将这些方法作为自定义验证器
+    # 并在已设置验证器之后调用它们
+    # 确保用户输入的username和email不会与数据库中已存在的数据冲突
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
--- app/routes.py
+++ app/routes.py
@@ -2,14 +2,23 @@ from flask import render_template
 from flask import flash
 from flask import redirect
 from flask import url_for
+from flask_login import current_user
+from flask_login import login_user
+from flask_login import logout_user
+from flask_login import login_required
+from flask import request
+from werkzeug.urls import url_parse
 
 from app import app
 from app.forms import LoginForm
+from app.models import User
+from app import db
+from app.forms import RegistrationForm
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Miguel'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -20,13 +29,52 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # current_user变量的值可以是数据库中的一个用户对象
+    # Flask-Login 通过 models.py 提供的用户加载函数回调读取
+    # 当用户已经登录，重定向到主页
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        user = User.query.filter_by(username=form.username.data).first()
+        # 验证用户名可能是否有效，或者用户密码是否正确
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        # login_user() 该函数会将用户登录状态注册为已登录
+        # 这意味着用户导航到任何未来的页面时
+        # 应用都会将用户实例赋值给 current_user 变量
+        login_user(user, remember=form.remember_me.data)
+        # 读取和处理 URL 中添加的 next 查询字符串参数
+        # 应用就可以在登录后使用它来重定向
+        next_page = request.args.get('next')
+        # 防止 next 参数中插入一个指向恶意站点的URL
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        # next 参数其值是一个相对路径
+        # 将会重定向到本应用的这个相对路径
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)   
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)     
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))    
